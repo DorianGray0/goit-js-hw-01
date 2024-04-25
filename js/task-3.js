0b6ea3d92b90bf161731ab26c659a449fe25f6a3
DorianGray0 <d.boyko0509@gmail.com>
2024-04-25 13:46:53 +0300

    fixe hw - 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,5 @@
 function getElementWidth(content, padding, border) {
-  const toNumber = Number.parseInt(content) + Number.parseInt(padding) + Number.parseInt(border);
+  const toNumber =
+    Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2;
   return toNumber;
 }
